7374d217852aa7e5454d42c1849ec628434d13a5
illilllill337 <84819741+Justin773@users.noreply.github.com>
2024-10-01 20:00:10 +0000

    assignment1

---
+++ assignments/assignment1.py
@@ -0,0 +1,2 @@
+def convert(a): a.replace(":)", "🙂").replace(":(", "🙁") 
+print(input("").lower() + "\n" + input().replace(" ", "...") + "\n" + convert(input()) + "\n" + str(int(input()) * (3*10**8)**2) )
