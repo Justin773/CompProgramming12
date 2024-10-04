204304c010b4f3c4902ae3430a800a6c1ced0e71
illilllill337 <84819741+Justin773@users.noreply.github.com>
2024-10-04 09:18:03 -0700

    Add files via upload

--- assignments/Justin_Kim_Functions_Lists_Tuples.py
+++ assignments/Justin_Kim_Functions_Lists_Tuples.py
@@ -1,13 +1,50 @@
-zoo_animals = []
+def create_zoo():
+    
+    print("\033[2J\033[;H")
+  
+    zoo_animals = [
+                   input(f"Zoo Animal {i+1}: ").lower()
+                       for i in range(5)
+                  ]
+    
+    print('\n' + '_'*20 + '\n')
 
-for i in range(5):
-    animal = input(f"Enter species of animal {i+1}: ").lower()  
-    zoo_animals.append(animal)
+    print("Zoo:", end="\n    ")
+   
+    print(*zoo_animals, sep="\n    ", end="\n\n")
 
-print("Zoo Animals:", zoo_animals)
+    for animal in set(zoo_animals):
+        amount = zoo_animals.count(animal)
+        is_plural = amount!=1
+        print(f"There {'are' if is_plural else 'is'} {amount} {animal}{'s' if is_plural else ''}")
 
-lion_count = zoo_animals.count("lion")
-panda_count = zoo_animals.count("panda")
-bear_count = zoo_animals.count("bear")
 
-print(f"Lions: {lion_count}, Pandas: {panda_count}, Bears: {bear_count}")
+def colour_converter():
+
+    print("\033[2J\033[;H")
+
+    r = int(input("Red: "))
+    g = int(input("Green: "))
+    b = int(input("Blue: "))
+    rgb = (r, g, b)
+
+    print(convert_rgb_to_hex(rgb))
+
+
+def convert_rgb_to_hex(rgb):
+    return "%02x%02x%02x" % rgb
+
+def main():
+    while True:
+        input_ = input("Zoo or Colour or quit: ").lower()
+        if input_ == "zoo":
+            create_zoo()
+        elif input_ == "colour":
+            colour_converter()
+        elif input_ == "quit":
+            return
+        else:
+            print("Not an option buddy")
+
+if __name__ == "__main__":
+     main()
